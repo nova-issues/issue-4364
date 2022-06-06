21838e42b015d35fd6259776ed6f34024559cfe0
Mior Muhammad Zaki <crynobone@gmail.com>
2022-06-06 11:48:12 +0800

    Add reproducing code for laravel/nova-issues#4364
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ nova-components/CreditCheck/src/ToolServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Infinitypaul\CreditCheck;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+use Infinitypaul\CreditCheck\Http\Middleware\Authorize;
+
+class ToolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
+        Nova::serving(function (ServingNova $event) {
+            //
+        });
+    }
+
+    /**
+     * Register the tool's routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+
+        Nova::router(['nova', Authorize::class], 'credit-check')
+            ->group(__DIR__.'/../routes/inertia.php');
+
+        Route::middleware(['nova', Authorize::class])
+            ->prefix('nova-vendor/credit-check')
+            ->group(__DIR__.'/../routes/api.php');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
